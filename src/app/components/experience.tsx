4b592628f8e4e22166d8a31d2d051fb5d2c30f82
kaynot <percykaynot@gmail.com>
2024-06-10 18:12:59 +0000

    new changes

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -1,20 +1,55 @@
-import React from 'react'
-
+import React from "react";
+import { ClockCircleOutlined } from "@ant-design/icons";
+import { Timeline } from "antd";
+import { styleText } from "util";
 export const Experience = () => {
   return (
-    <div className='h-[700px]'>
-        <h1 className='text-center my-10'>Experience</h1>
-        <div className='flex justify-center'>
-            <div className='bg-green-200 w-[30%] h-[500px]'>
-            <h1>Company Name</h1>
-            <p>Position</p>
-            <p>Duration</p>
-            <p>Responsibilities</p>
-            </div>
-            <div className='bg-blue-200 w-[30%] '>
-            <h1>Company Name</h1>
-            </div>
+    <div className="h-[700px]">
+      <h1 className="text-center my-10 text-bold font-mono text-[30px]">
+        Career
+      </h1>
+      <div className="flex justify-center">
+        <div className=" w-[30%] h-[500px]">
+          <h1 className="text-center text-bold text-[20px]">Education</h1>
+          <Timeline
+            mode="alternate"
+            className="text-white mt-10 text-[20px] "
+            items={[
+              {
+                children: " Started SHS at Ofori Panin Senior High School in 2016 ",
+                color: "green",
+                style:{border: "1px solid green", padding: "10px", backgroundColor:"pink",color:"black"}
+              },
+              {
+                children: "Completed SHS in 2019",
+                color: "red",
+                style:{ padding: "10px"}
+              },
+              {
+                children: `Begun University at University of Professional Sudies Accra (UPSA) in 2019`,
+                color: "green",
+                style:{border: "1px solid green", padding: "10px", backgroundColor:"pink",color:"black"}
+              },
+              {
+                color: "red",
+                children: "Network problems being solved 2015-09-01",
+              },
+              {
+                children: "Create a services site 2015-09-01",
+              },
+              {
+                dot: <ClockCircleOutlined style={{ fontSize: "16px" }} />,
+                children: "Technical testing 2015-09-01",
+              },
+            ]}
+            
+          />
+
         </div>
+        <div className="bg-blue-200 w-[30%] ">
+          <h1 className="text-center text-bold text-[20px]">Work Experience</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import Image from "next/image";
 import img from "./images/file.png";
-import man from './images/Premium_Photo___Fun_3D_cartoon_illustration_of_an_indian_businessman-removebg-preview.png'
+import man from "./images/Premium_Photo___Fun_3D_cartoon_illustration_of_an_indian_businessman-removebg-preview.png";
 import { AiOutlineDownload } from "react-icons/ai";
 export const Intro = () => {
   return (
@@ -14,7 +14,7 @@ export const Intro = () => {
           <FaLinkedin size={32} color="pink" className="lg:text-4xl " />
         </div>
         <div>
-        <Image src={man} alt="cartoon" className="w-[400px] h-[500px] " />
+          <Image src={man} alt="cartoon" className="w-[400px] h-[500px] " />
         </div>
         <div className=" w-[80%] flex flex-col justify-center h-full mx-10 ">
           <h3 className=" font-bold  font-mono  ">Welcome to my Portfolio</h3>
@@ -26,10 +26,14 @@ export const Intro = () => {
           <a
             href="src\app\components\File\Percy-Djiwornu-Konotey-FlowCV-Resume-20240523.pdf"
             download
-            className=" bg-gray-900  font-bold font-mono p-2 border border-green-300 text-white rounded-3xl mt-2 flex items-center w-[12%] gap-2"
+            className=" bg-gray-900  font-bold font-mono p-2 border border-green-300 text-white rounded-3xl mt-2 flex items-center w-[12%] gap-2  hover:bg-pink-200  hover:text-black transition duration-500 transform hover:scale-110"
           >
             Download CV
-            <AiOutlineDownload size={20} color="white" className="lg:text-4xl animate-bounce"  />
+            <AiOutlineDownload
+              size={20}
+              color="white"
+              className="lg:text-4xl animate-bounce"
+            />
           </a>
         </div>
         <div className="h-[70%] "></div>
