5c9231a2dc91debac0f100e17aa401dfde9dd5f2
kaynot <percykaynot@gmail.com>
2024-06-10 22:19:59 +0000

    half way thru

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -4,11 +4,11 @@ import { Timeline } from "antd";
 import { styleText } from "util";
 export const Experience = () => {
   return (
-    <div className="h-[700px]">
+    <div className="h-[500px]">
       <h1 className="text-center my-10 text-bold font-mono text-[30px]">
         Career
       </h1>
-      <div className="flex justify-center">
+      <div className="flex justify-center gap-4">
         <div className=" w-[30%] h-[500px]">
           <h1 className="text-center text-bold text-[20px]">Education</h1>
           <Timeline
@@ -16,38 +16,62 @@ export const Experience = () => {
             className="text-white mt-10 text-[20px] "
             items={[
               {
-                children: " Started SHS at Ofori Panin Senior High School in 2016 ",
+                children: " Started Senior High School at Ofori Panin Senior High School in 2016 ",
                 color: "green",
-                style:{border: "1px solid green", padding: "10px", backgroundColor:"pink",color:"black"}
+                style:{border: "1px solid green", padding: "10px",}
               },
               {
-                children: "Completed SHS in 2019",
+                children: "Completed Senior High in 2019",
                 color: "red",
+                style:{ padding: "10px",}
+              },
+              {
+                children: `Began Tertiary Education at University of Professional Sudies Accra (UPSA) in 2019`,
+                color: "green",
+                style:{border: "1px solid green", padding: "10px",}
+              },
+              {
+                color: "red",
+                children: "Completed Tertiary Education in 2023",
                 style:{ padding: "10px"}
               },
-              {
-                children: `Begun University at University of Professional Sudies Accra (UPSA) in 2019`,
-                color: "green",
-                style:{border: "1px solid green", padding: "10px", backgroundColor:"pink",color:"black"}
-              },
-              {
-                color: "red",
-                children: "Network problems being solved 2015-09-01",
-              },
-              {
-                children: "Create a services site 2015-09-01",
-              },
-              {
-                dot: <ClockCircleOutlined style={{ fontSize: "16px" }} />,
-                children: "Technical testing 2015-09-01",
-              },
+              
             ]}
             
           />
 
         </div>
-        <div className="bg-blue-200 w-[30%] ">
+        <div className=" w-[30%] ">
           <h1 className="text-center text-bold text-[20px]">Work Experience</h1>
+          <Timeline
+            mode="alternate"
+            className="text-white mt-10 text-[20px] "
+            items={[
+              {
+                children: " Did my Internship at the Ghana Prisons Headquaters- 09/2023 ",
+                color: "green",
+                style:{border: "1px solid green", padding: "10px",}
+              },
+              {
+                children: "Completed Internship in month -10/2023",
+                color: "red",
+                style:{ padding: "10px",}
+              },
+              {
+                children: `Started working at Innorik Ghana LTD as a national service personnel in 2023/2024`,
+                color: "green",
+                style:{border: "1px solid green", padding: "10px",}
+              },
+              {
+                color: "green",
+                children: "Still working at Innorik Ghana LTD as a national service personnel ",
+                style:{ padding: "10px"}
+              },
+              
+            ]}
+            
+          />
+
         </div>
       </div>
     </div>
--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,21 +1,36 @@
 import React from 'react'
+import Image from "next/image";
+import todo from './images/todo.png'
+import kids from './images/bookstore.png'
+import portfolio from './images/portfolio.png'
 
 export const Projects = () => {
     const projects = [
         {
             id:0,
-            title: 'Project 1',
-            description: 'This is a project description'
-        }
+            title: 'Todolist App',
+            img:todo
+        },
+        {
+            id:1,
+            title: 'Kids Bookstore App',
+            img:kids
+        },
+        {
+            id:2,
+            title: 'Portfolio',
+            img:portfolio
+        },
     ]
   return (
-    <div className='h-[700px]'>
-        <h1 className='text-center'>Featured Projects</h1>
-        <div className='flex justify-center mt-10'>
-        {projects.map(({id, title, description}) => (
-            <div key={id.toString()} className='bg-pink-200 shadow-2xl rounded-xl p-4 h-[600px] w-[500px] border-green-300 border '>
-                <h1 className='text-xl font-bold underline'>{title}</h1>
-                <p className='text-sm'>{description}</p>
+    <div className='h-[700px] mx-4'>
+        <h1 className='text-center font-bold text-[30px] font-mono'>Featured Projects</h1>
+        <div className='flex justify-center mt-10 gap-3'>
+        {projects.map(({id, title,img }) => (
+            <div key={id.toString()} className='shadow-2xl rounded-xl p-4 h-[600px] w-[40%] border-green-300 border '>
+                <h1 className='text-xl font-bold text-center'>{title}</h1>
+                <Image src={img} alt="project" className='w-[100%] h-[95%]  p-10' />
+                
             </div>
         ))}
 </div>
