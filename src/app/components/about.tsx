bfdb979ca59d56baa330638afbd82134fb445d86
kaynot <percykaynot@gmail.com>
2024-06-14 18:35:13 +0000

    kkd

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -21,10 +21,10 @@ export const About = () => {
         {/* <!-- Text Section --> */}
         <div className="h-auto lg:h-[500px] w-full lg:w-[50%] flex flex-col justify-center">
             <div className="text-[15px] p-4 lg:p-0">
-                I'm a passionate front-end developer with a love for creating beautiful and functional web applications. With expertise in React, TypeScript, Tailwind CSS, and JavaScript. 
+                I&apos;m a passionate front-end developer with a love for creating beautiful and functional web applications. With expertise in React, TypeScript, Tailwind CSS, and JavaScript. 
                 <p>I build responsive and user-friendly websites that provide seamless experiences across all devices. I enjoy solving problems and continuously learning new technologies to keep my skills up-to-date.</p> 
-                When I'm not coding, you can find me exploring the latest web design trends or working on personal projects. 
-                <p className="mt-2 font-bold text-2xl">Let's build something amazing together!</p>
+                When I&apos;m not coding, you can find me exploring the latest web design trends or working on personal projects. 
+                <p className="mt-2 font-bold text-2xl">Let&apos;s build something amazing together!</p>
             </div>
             <div className="flex gap-5 mt-4 lg:mt-10 justify-center lg:justify-start">
             <a href="https://github.com/kaynot" target="_blank" rel="noopener noreferrer">
